d6065922f1597061dbb042a86974b7a61fd1631f
ibeliaie <ibeliaie@student.42wolfsburg.de>
2024-04-17 17:13:11 +0200

    tokens??

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -3,24 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibeliaie <ibeliaie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ibeliaie <ibeliaie@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/15 17:15:34 by ibeliaie          #+#    #+#             */
-/*   Updated: 2024/04/15 17:58:45 by ibeliaie         ###   ########.fr       */
+/*   Created: 2024/04/17 16:04:11 by ibeliaie          #+#    #+#             */
+/*   Updated: 2024/04/17 17:12:35 by ibeliaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft/libft.h"
 #include "lexer.h"
 
-t_word *new_token(char *content, int index, t_word_type type)
+//allocate memory for new token, copy the content, init other fields (index, type)
+t_word	*token_new(char *content, int index, t_word_type type)
 {
-	t_word *token;
-
+	t_word	*token;
+	
 	token = (t_word *)malloc(sizeof(t_word));
 	if (!token)
 		return (NULL);
 	token->content = ft_strdup(content);
+	if (!token->content)
+	{
+		free(token);
+		return (NULL);
+	}
 	token->index = index;
 	token->type = type;
 	token->next = NULL;
@@ -28,60 +34,122 @@ t_word *new_token(char *content, int index, t_word_type type)
 	return (token);
 }
 
-t_word *create_token(t_split *tokens, char *start, int i, t_word_type type)
+//create new token, add it to the list and update the first and last pointers
+t_word	*token_create(t_split *tokens, char *start, int index, t_word_type type)
 {
-	t_word *token = new_token(start, i, type);
-	if (tokens->last)
+	t_word	*token;
+
+	token = token_new(start, index, type);
+	if (!token)
 	{
-		tokens->last->next = token;
-		token->prev = tokens->last;
-		tokens->last = token;
+		free(tokens);
+		return (NULL);
+	}
+	if (!tokens->head)
+	{
+		tokens->head = token;
+		tokens->tail = token;
 	}
 	else
 	{
-		tokens->first = token;
-		tokens->last = token;
+		token->prev = tokens->tail;
+		tokens->tail->next = token;
+		tokens->tail = token;
 	}
-	return token;
+	return (token);
 }
 
-void handle_token(t_split *tokens, char **start, char *s, int *i, t_word_type type)
+//init the info struct with pointers to the start of the string, the string itself and the current index
+void	token_info_init(t_info *info, char **start, char *s, int *i)
 {
-	if (*start != &s[*i])
-	{
-		if (type == CMD)
-			create_token(tokens, *start, *i, CMD);
-		else
-			create_token(tokens, *start, *i, OPERATOR);
-	}
-	create_token(tokens, &s[*i], *i, type);
-	*start = &s[*i + 1];
+	t_split	*tokens;
+	
+	tokens->cmds = 0;
+	tokens->first = NULL;
+	tokens->last = NULL;
+	info->start = start;
+	info->s = s;
+	info->i = i;
 }
 
-t_split *tokenize(char *s)
+//determine the type of the token and create it
+void	token_handle(t_split *tokens, t_info *info, t_word_type type)
 {
-	t_split *tokens;
-	char *start;
-	int i;
+	if (*(info->start) != &(info->s)[*(info->i)])
+		token_create(tokens, *(info->start), *(info->i), type);
+	else
+		token_create(tokens, &(info->s)[*(info->i)], *(info->i), type);
+	*(info->start) = &(info->s)[*(info->i) + 1];
+}
+
+//tokenize the input string: iterate over the string and call token_handle when encoutering a space or an operator
+t_split	*tokenize(char *s)
+{
+	t_info	info;
+	t_split	*tokens;
+	char	*start;
+	int		i;
 
 	tokens = (t_split *)malloc(sizeof(t_split));
 	if (!tokens)
 		return (NULL);
-	tokens->cmds = 0;
-	tokens->first = NULL;
-	tokens->last = NULL;
-	start = s;
 	i = 0;
+	start = s;
+	token_info_init(&info, &start, s, &i);
 	while (s[i])
 	{
 		if (is_space(s[i]) || s[i] == '|' || s[i] == '<' || s[i] == '>')
 		{
-			handle_token(tokens, &start, s, &i, CMD);
-			handle_token(tokens, &start, s, &i, OPERATOR);
+			token_handle(tokens, &info, CMD);
+			token_handle(tokens, &info, OPERATOR);
 		}
 		i++;
 	}
 	if (start != &s[i])
-		create_token(tokens, start, i, CMD);
+		token_create(tokens, start, i, CMD);
 	return (tokens);
 }
+
+// int main()
+// {
+// 	t_word *token = token_new("test", 1, WORD);
+//     if (token == NULL)
+// 	{
+//         printf("Failed to create token\n");
+//         return (1);
+//     }
+//     if (strcmp(token->content, "test") != 0)
+// 	{
+//         printf("Content is not set correctly\n");
+//         return (1);
+//     }
+//     if (token->index != 1)
+// 	{
+//         printf("Index is not set correctly\n");
+//         return (1);
+//     }
+//     if (token->type != WORD)
+// 	{
+//         printf("Type is not set correctly\n");
+//         return (1);
+//     }
+//     if (token->next != NULL)
+// 	{
+//         printf("Next is not set correctly\n");
+//         return (1);
+//     }
+//     if (token->prev != NULL)
+// 	{
+//         printf("Prev is not set correctly\n");
+//         return (1);
+//     }
+//     printf("token_new passed all tests\n");
+//     t_split *tokens = (t_split *)malloc(sizeof(t_split));
+//     t_word *token2 = token_create(tokens, "test2", 2, WORD);
+//     if (token2 == NULL) {
+//         printf("Failed to create token\n");
+//         return (1);
+//     }
+//     printf("token_create passed all tests\n");
+//     return (0);
+// }
--- lexer/lexer.h
+++ lexer/lexer.h
@@ -3,15 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibeliaie <ibeliaie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ibeliaie <ibeliaie@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 17:15:42 by ibeliaie          #+#    #+#             */
-/*   Updated: 2024/04/15 17:58:55 by ibeliaie         ###   ########.fr       */
+/*   Updated: 2024/04/17 17:06:59 by ibeliaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <unistd.h>
 
 typedef struct s_word	t_word;
 
@@ -20,6 +25,8 @@ typedef struct s_split
 	int		cmds;
 	t_word	*first;
 	t_word	*last;
+	t_word *head;
+	t_word *tail;
 }	t_split;
 
 typedef struct s_word
@@ -31,8 +38,16 @@ typedef struct s_word
 	struct s_word	*prev;
 }	t_word;
 
+typedef struct s_info
+{
+	char	**start;
+	char	*s;
+	int		*i;
+}               t_info;
+
 typedef enum e_word_type
 {
+	WORD,
 	CMD,
 	OPERATOR,
 	FLAG,
@@ -46,10 +61,12 @@ typedef enum e_word_type
 
 
 //creating tokens and stuff
-t_word *new_token(char *content, int index, t_word_type type);
-t_word *create_token(t_split *tokens, char *start, int i, t_word_type type);
-void handle_token(t_split *tokens, char **start, char *s, int *i, t_word_type type);
-t_split *tokenize(char *s);
+t_word	*token_new(char *content, int index, t_word_type type);
+t_word	*token_create(t_split *tokens, char *start, int index, t_word_type type);
+t_info	*init_info(char **start, char *s, int *i);;
+t_word	*create_token(t_split *tokens, char *start, int i, t_word_type type);
+void	handle_token(t_split *tokens, char **start, char *s, int *i, t_word_type type);
+t_split	*tokenize(char *s);
 
 //utils
 int	is_space(char c);
