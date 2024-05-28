f24740c58a6ab30042c78f692ece663eaa738b70
ibeliaie <ibeliaie@student.42wolfsburg.de>
2024-05-28 17:07:58 +0200

    updated strjoin to work better in this case

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -13,29 +13,26 @@
 #include "libft.h"
 
 /* combine 2 strings into single string */
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	int		i;
 	int		j;
-	char	*str;
+	char	*p;
 
-	if (!s1 ||!s2)
-		return (0);
+	p = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!p)
+		return ((void *)0);
 	i = 0;
 	j = 0;
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!str)
-		return (0);
-	while (s1[i])
+	while (s1 && s1[i])
 	{
-		str[i] = s1[i];
-		i++;
+		p[i] = s1[i];
+		++i;
 	}
 	while (s2[j])
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	return (str);
+		p[i++] = s2[j++];
+	p[i] = '\0';
+	free(s1);
+	free(s2);
+	return (p);
 }
