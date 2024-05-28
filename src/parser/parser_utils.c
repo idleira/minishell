a831c2d9a08d25751f658e5180f63094a6da4d0a
ibeliaie <ibeliaie@student.42wolfsburg.de>
2024-05-28 17:09:03 +0200

    commented out unused for now functions

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -13,24 +13,23 @@
 #include "../../inc/minishell.h"
 
 // counts the number of file redirections in the parser list
-int	count_file_red(t_dlist	*head)
-{
-	t_dlist	*temp;
-	int		number;
+// int	count_files(t_dlist	*head)
+// {
+// 	t_dlist	*temp;
+// 	int		number;
 
-	temp = head;
-	number = 0;
-	while (temp && temp->type != __PIPE)
-	{
-		if (temp->type == __RED_IN || temp->type == __RED_OUT
-			|| temp->type == __RED_APP || temp->type == __HEREDOC)
-			++number;
-		temp = temp->next;
-	}
-	return (number);
-}
+// 	temp = head;
+// 	number = 0;
+// 	while (temp && temp->type != __PIPE)
+// 	{
+// 		if (temp->type == __RED_IN || temp->type == __RED_OUT
+// 			|| temp->type == __RED_APP || temp->type == __HEREDOC)
+// 			++number;
+// 		temp = temp->next;
+// 	}
+// 	return (number);
+// }
 
-// counts the number of arguments in the parser list
 int	count_args(t_dlist *head)
 {
 	t_dlist	*temp;
@@ -49,11 +48,11 @@ int	count_args(t_dlist *head)
 }
 
 // assigns the file redirections to the parser struct
-void	assign_file_red(t_dlist *head, t_parser *node, int i)
-{
-	node->file[i].type = head->type;
-	node->file[i].name = ft_strdup(head->next->value);
-}
+// void	assign_file(t_dlist *head, t_parser *node, int i)
+// {
+// 	node->file[i].type = head->type;
+// 	node->file[i].name = ft_strdup(head->next->value);
+// }
 
 void	assign_args(t_dlist *head, t_parser *node, char *args)
 {
