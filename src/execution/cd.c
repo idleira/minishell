b096afe9701ecf6698d4fe45e48bf7ef86383002
Marianna Zhukova <mzhukova@c2r4s7.42wolfsburg.de>
2024-06-21 17:01:34 +0200

    cd - doesnt work if previous cmd was incorrect

--- src/execution/cd.c
+++ src/execution/cd.c
@@ -6,7 +6,7 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 19:27:54 by mzhukova          #+#    #+#             */
-/*   Updated: 2024/06/21 16:05:37 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/06/21 16:59:46 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ int check_builtins(t_parser *cmd, t_env *env)
 	//first check with first arg, then check how to fix it so it works from every position
 	if (ft_strnstr(cmd->args[0], "cd", 2))
 	{
+		if (ft_strlen(cmd->args[0]) > 2)
+		{
+			printf("command not found\n");
+			return (1);
+		}
 		change_directory(cmd, env);
 		return (1);
 	}
@@ -34,7 +39,14 @@ void change_directory(t_parser *cmd, t_env *env)
 
 	int i = 0;
 	cd_path = "";
-	
+	if (!cmd->args[1] || (cmd->args[1][0] == '~' && !cmd->args[2]))
+	{
+		env->old_pwd = env->pwd;
+		env->pwd = env->home;
+		chdir(env->home);
+		return ;
+		
+	}
 	while (cmd->args[i])
 	{
 		if (ft_strnstr(cmd->args[i], "cd", 2) == 0)
@@ -42,15 +54,17 @@ void change_directory(t_parser *cmd, t_env *env)
 			if (ft_strnstr(cmd->args[1], "-", 1) != 0)
 			{
 				printf("%s\n", env->old_pwd);
-				chdir(env->old_pwd);
+				if (chdir(env->old_pwd) != 0)
+					printf("No such file or directory\n");
 				env->pwd = env->old_pwd;
 				return ;
 			}
+
 			cd_path = my_strjoin(cd_path, cmd->args[i]);
 		}
 		i++;
 	}
-	env->old_pwd = env->pwd;
+	//env->old_pwd = env->pwd;
 	if (cd_path[0] != '/')
 		cd_path = my_strjoin("/", cd_path);
 	if (cd_path[ft_strlen(cd_path)] != '/')
@@ -59,8 +73,11 @@ void change_directory(t_parser *cmd, t_env *env)
 	if (chdir(cd_path) == 0)
     {
         if (getcwd(buffer, sizeof(buffer)) != NULL)
-        {
-            env->pwd = ft_strdup(buffer); //ree this later
-        }
+		{
+        	env->pwd = ft_strdup(buffer); //free this later
+		}
     }
+	else
+		printf("No such file or directory, return\n");
+	env->old_pwd = env->pwd;
 }
