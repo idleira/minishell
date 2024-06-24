8076aa45874c0d5e4f338ec6c4153e21cc268d08
Marianna Zhukova <mzhukova@c1r17s3.42wolfsburg.de>
2024-06-24 16:56:49 +0200

    cd works, do tests later

--- src/execution/cd.c
+++ src/execution/cd.c
@@ -6,7 +6,7 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 19:27:54 by mzhukova          #+#    #+#             */
-/*   Updated: 2024/06/21 16:59:46 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/06/24 16:52:58 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,8 @@
 // Before execution check_builtins() checks the keyword and then calls the needed builtin function
 int check_builtins(t_parser *cmd, t_env *env)
 {
-	//first check with first arg, then check how to fix it so it works from every position
-	if (ft_strnstr(cmd->args[0], "cd", 2))
+	if (cmd->args[0] && ft_strncmp(cmd->args[0], "cd", 2) == 0)
 	{
-		if (ft_strlen(cmd->args[0]) > 2)
-		{
-			printf("command not found\n");
-			return (1);
-		}
 		change_directory(cmd, env);
 		return (1);
 	}
@@ -34,50 +28,47 @@ int check_builtins(t_parser *cmd, t_env *env)
 
 void change_directory(t_parser *cmd, t_env *env)
 {
-	char	*cd_path;
-	char	buffer[4096];
+	char *cd_path;
+	char buffer[4096];
 
-	int i = 0;
-	cd_path = "";
-	if (!cmd->args[1] || (cmd->args[1][0] == '~' && !cmd->args[2]))
+	// Handle 'cd' with no arguments or 'cd ~'
+	if (!cmd->args[1] || (cmd->args[1][0] == '~' && !cmd->args[1][1]))
 	{
 		env->old_pwd = env->pwd;
-		env->pwd = env->home;
-		chdir(env->home);
-		return ;
-		
+		if (chdir(env->home) == 0)
+			env->pwd = ft_strdup(env->home);
+		else
+			perror("chdir");
+		return;
 	}
-	while (cmd->args[i])
-	{
-		if (ft_strnstr(cmd->args[i], "cd", 2) == 0)
-		{
-			if (ft_strnstr(cmd->args[1], "-", 1) != 0)
-			{
-				printf("%s\n", env->old_pwd);
-				if (chdir(env->old_pwd) != 0)
-					printf("No such file or directory\n");
-				env->pwd = env->old_pwd;
-				return ;
-			}
 
-			cd_path = my_strjoin(cd_path, cmd->args[i]);
-		}
-		i++;
-	}
-	//env->old_pwd = env->pwd;
-	if (cd_path[0] != '/')
-		cd_path = my_strjoin("/", cd_path);
-	if (cd_path[ft_strlen(cd_path)] != '/')
-		cd_path = my_strjoin(cd_path, "/");
-	cd_path = my_strjoin(env->pwd, cd_path);
-	if (chdir(cd_path) == 0)
-    {
-        if (getcwd(buffer, sizeof(buffer)) != NULL)
+	// Handle 'cd -'
+	if (ft_strncmp(cmd->args[1], "-", 1) == 0)
+	{
+		printf("%s\n", env->old_pwd);
+		if (chdir(env->old_pwd) == 0)
 		{
-        	env->pwd = ft_strdup(buffer); //free this later
+			char *temp = env->pwd;
+			env->pwd = env->old_pwd;
+			env->old_pwd = temp;
 		}
-    }
+		else
+			perror("chdir");
+		return;
+	}
+
+	// Construct the target path
+	cd_path = cmd->args[1];
+	if (chdir(cd_path) == 0)
+	{
+		if (getcwd(buffer, sizeof(buffer)) != NULL)
+		{
+			env->old_pwd = env->pwd;
+			env->pwd = ft_strdup(buffer); // Remember to free old env->pwd later
+		}
+		else
+			perror("getcwd");
+	}
 	else
-		printf("No such file or directory, return\n");
-	env->old_pwd = env->pwd;
+		printf("No such file or directory\n");
 }
