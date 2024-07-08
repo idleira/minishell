ebea715a687a9066a31dda25935824bcb21f4bf1
ibeliaie <ibeliaie@student.42wolfsburg.de>
2024-07-08 14:59:59 +0200

    removed debug text

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: ibeliaie <ibeliaie@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 16:52:34 by ibeliaie          #+#    #+#             */
-/*   Updated: 2024/06/24 17:24:44 by ibeliaie         ###   ########.fr       */
+/*   Updated: 2024/07/08 14:57:25 by ibeliaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 // sets up signal handlers for SIGINT(request to interrupt the process) and SIGQUIT(request to quit the process)
 void signal_handlers_setup(void)
 {
-	printf("signal_handlers_setup\n");
 	signal(SIGINT, signal_handler);
 	signal(SIGQUIT, SIG_IGN);
 }
@@ -25,7 +24,6 @@ void	signal_handler(int signum)
 {
 	if (signum == SIGINT)
 	{
-		printf("ctrl+c pressed\n");
 		write(1, "\n", 1);
 		rl_on_new_line();
 		rl_replace_line("", 0);
